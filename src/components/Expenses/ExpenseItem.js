1f2bbadd9ad1f85e57fdc8a59ddea87e1b599878
Hum Bruno <hsbruno1@gmail.com>
2022-03-31 14:46:44 +0100

    new expense component

--- src/components/Expenses/ExpenseItem.js
+++ src/components/Expenses/ExpenseItem.js
@@ -1,8 +1,15 @@
+import { useState } from "react";
 import ExpenseDate from "./ExpenseDate";
 import Card from "../UI/Card";
 import "./ExpenseItem.css";
 
-function ExpenseItem({ title, amount, date }) {
+function ExpenseItem({ itemTitle, amount, date }) {
+  const [title, setTitle] = useState(itemTitle);
+
+  const clickHandler = () => {
+    setTitle("New title");
+  };
+
   return (
     <Card className="expense-item">
       <ExpenseDate date={date} />
@@ -10,6 +17,7 @@ function ExpenseItem({ title, amount, date }) {
         <h2>{title}</h2>
         <div className="expense-item__price">${amount}</div>
       </div>
+      <button onClick={clickHandler}>Change title</button>
     </Card>
   );
 }
--- src/components/Expenses/Expenses.js
+++ src/components/Expenses/Expenses.js
@@ -6,22 +6,22 @@ function Expenses({ items }) {
   return (
     <Card className="expenses">
       <ExpenseItem
-        title={items[0].title}
+        itemTitle={items[0].title}
         amount={items[0].amount}
         date={items[0].date}
       />
       <ExpenseItem
-        title={items[1].title}
+        itemTitle={items[1].title}
         amount={items[1].amount}
         date={items[1].date}
       />
       <ExpenseItem
-        title={items[2].title}
+        itemTitle={items[2].title}
         amount={items[2].amount}
         date={items[2].date}
       />
       <ExpenseItem
-        title={items[3].title}
+        itemTitle={items[3].title}
         amount={items[3].amount}
         date={items[3].date}
       />
