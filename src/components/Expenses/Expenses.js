a2404e19a1938d3347df91a4de877d982e8d02bd
Hum Bruno <hsbruno1@gmail.com>
2022-04-05 19:53:46 +0100

    char bar logic and styles

--- src/components/Expenses/Expenses.js
+++ src/components/Expenses/Expenses.js
@@ -1,8 +1,9 @@
 import "./Expenses.css";
-import ExpenseItem from "./ExpenseItem";
 import Card from "../UI/Card";
 import ExpensesFilter from "./ExpensesFilter";
 import { useState } from "react";
+import ExpensesList from "./ExpensesList";
+import ExpensesChart from "./ExpensesChart";
 
 function Expenses({ items }) {
   const [filteredYear, setFilteredYear] = useState("2020");
@@ -10,33 +11,20 @@ function Expenses({ items }) {
   const filterChangeHandler = (selectedYear) => {
     setFilteredYear(selectedYear);
   };
+
+  const filteredExpenses = items.filter((expense) => {
+    return expense.date.getFullYear().toString() === filteredYear;
+  });
+
   return (
     <div>
       <Card className="expenses">
+        <ExpensesChart expenses={filteredExpenses} />
         <ExpensesFilter
           selected={filteredYear}
           onChangeFilter={filterChangeHandler}
         />
-        <ExpenseItem
-          title={items[0].title}
-          amount={items[0].amount}
-          date={items[0].date}
-        />
-        <ExpenseItem
-          title={items[1].title}
-          amount={items[1].amount}
-          date={items[1].date}
-        />
-        <ExpenseItem
-          title={items[2].title}
-          amount={items[2].amount}
-          date={items[2].date}
-        />
-        <ExpenseItem
-          title={items[3].title}
-          amount={items[3].amount}
-          date={items[3].date}
-        />
+        <ExpensesList items={filteredExpenses} />
       </Card>
     </div>
   );
